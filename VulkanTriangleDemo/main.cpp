b268d5cd5db5d247b4f11a40e6c1ffe43775ad7a
Euclid Skyline <euclid.skyline@gmail.com>
2025-06-07 20:39:37 +0300

    Integrate Vulkan instance into HelloTriangleApplication
    Added a Vulkan instance variable and implemented the
    createInstance method for Vulkan initialization. Updated
    initVulkan to call createInstance and modified cleanup
    to properly destroy the Vulkan instance before GLFW
    cleanup, enhancing graphics rendering capabilities.

--- VulkanTriangleDemo/main.cpp
+++ VulkanTriangleDemo/main.cpp
@@ -22,6 +22,7 @@ public:
 
 private:
     GLFWwindow* window;
+    VkInstance instance;
 
     void initWindow() {
         glfwInit();         // initializes the GLFW library
@@ -33,7 +34,7 @@ private:
     }
 
     void initVulkan() {
-        
+        createInstance();
     }
 
     void mainLoop() {
@@ -44,9 +45,40 @@ private:
     }
 
     void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
+    }
+
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
     }
 };
 
