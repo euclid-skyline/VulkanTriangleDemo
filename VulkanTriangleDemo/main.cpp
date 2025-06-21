e74037f22856f7874743cca9ed7db2c8d48c89a7
Euclid Skyline <euclid.skyline@gmail.com>
2025-06-21 23:04:36 +0300

    Add framebuffer management for swap chain
    - Introduced `std::vector<VkFramebuffer> swapChainFramebuffers` to manage framebuffers.
    - Implemented `createFramebuffers()` to initialize and create framebuffers for each image view.
    - Updated initialization sequence to call `createFramebuffers()` after setting up the graphics pipeline.
    - Enhanced `cleanup()` to properly destroy the created framebuffers.

--- VulkanTriangleDemo/main.cpp
+++ VulkanTriangleDemo/main.cpp
@@ -90,6 +90,7 @@ private:
     VkRenderPass renderPass;
     VkPipelineLayout pipelineLayout;
     VkPipeline graphicsPipeline;
+    std::vector<VkFramebuffer> swapChainFramebuffers;
 
     void initWindow() {
         glfwInit();         // initializes the GLFW library
@@ -110,6 +111,7 @@ private:
         createImageViews();
         createRenderPass();
         createGraphicsPipeline();
+        createFramebuffers();
     }
 
     void mainLoop() {
@@ -120,6 +122,10 @@ private:
     }
 
     void cleanup() {
+        for (auto framebuffer : swapChainFramebuffers) {
+            vkDestroyFramebuffer(device, framebuffer, nullptr);
+        }
+
         vkDestroyPipeline(device, graphicsPipeline, nullptr);
         vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
         vkDestroyRenderPass(device, renderPass, nullptr);
@@ -445,6 +451,29 @@ private:
 		// Clean up shader modules
         vkDestroyShaderModule(device, fragShaderModule, nullptr);
         vkDestroyShaderModule(device, vertShaderModule, nullptr);
+    }
+
+    void createFramebuffers() {
+        swapChainFramebuffers.resize(swapChainImageViews.size());
+
+        for (size_t i = 0; i < swapChainImageViews.size(); i++) {
+            VkImageView attachments[] = {
+                swapChainImageViews[i]
+            };
+
+            VkFramebufferCreateInfo framebufferInfo{};
+            framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+            framebufferInfo.renderPass = renderPass;
+            framebufferInfo.attachmentCount = 1;
+            framebufferInfo.pAttachments = attachments;
+            framebufferInfo.width = swapChainExtent.width;
+            framebufferInfo.height = swapChainExtent.height;
+            framebufferInfo.layers = 1;
+
+            if (vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create framebuffer!");
+            }
+        }
     }
 
     VkShaderModule createShaderModule(const std::vector<char>& code) {
